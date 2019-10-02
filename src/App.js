ca5d66691dee6710e0be4dd502d00fd6f1337bc7
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-10-01 20:30:24 -0700

    added header and instructions

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Components/Header'
+import Instructions from './Components/Instructions'
+
+class App extends React.Component {
+  render () {
+    return (
+      <>
+      <Header />
+      <Instructions />
+      </>
+    )
+  }
+}
+
+export default App
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = _ => {
+    return (
+        <div>
+            <h2 id='header'>Crystal Collector!</h2>
+        </div>
+    )
+}
+
+export default Header
