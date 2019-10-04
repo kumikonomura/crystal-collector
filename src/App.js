24a3efa328567d84e5f1a53f7863c27fb824b0bd
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-10-03 20:50:09 -0700

    implemented conditional statement in jewels function

--- src/App.js
+++ src/App.js
@@ -20,48 +20,179 @@ class App extends React.Component {
   // click jewel that is equal to a random number and that number will
   // appear on TOTAL SCORE
 
+  // handleClickDiamond = _ => {
+  //   console.log("click diamond working");
+  //   let diamond = this.state.diamond;
+  //   let userScore = this.state.userScore;
+  //   this.setState({
+  //     diamond: diamond,
+  //     userScore: userScore + diamond
+  //   });
+  //   console.log(diamond);
+  // };
+
   handleClickDiamond = _ => {
     console.log("click diamond working");
     let diamond = this.state.diamond;
     let userScore = this.state.userScore;
+    let gameScore = this.state.gameScore;
+    let wins = this.state.wins;
+    let losses = this.state.losses;
     this.setState({
       diamond: diamond,
       userScore: userScore + diamond
     });
     console.log(diamond);
+    if (userScore === gameScore) {
+      this.setState({
+        wins: wins + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+      window.alert("you win");
+    } else if (userScore > gameScore) {
+      this.setState({
+        losses: losses + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+      window.alert("you lose");
+    }
   };
+
+  // handleClickEmerald = _ => {
+  //   console.log("click emerald working");
+  //   let emerald = this.state.emerald;
+  //   let userScore = this.state.userScore;
+  //   this.setState({
+  //     emerald: emerald,
+  //     userScore: userScore + emerald
+  //   });
+  //   console.log(emerald);
+  // };
 
   handleClickEmerald = _ => {
     console.log("click emerald working");
     let emerald = this.state.emerald;
     let userScore = this.state.userScore;
+    let gameScore = this.state.gameScore;
+    let wins = this.state.wins;
+    let losses = this.state.losses;
     this.setState({
       emerald: emerald,
       userScore: userScore + emerald
     });
     console.log(emerald);
+    if (userScore === gameScore) {
+      this.setState({
+        wins: wins + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+    } else if (userScore > gameScore) {
+      this.setState({
+        losses: losses + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+      window.alert("you lose");
+    }
   };
+
+  // handleClickRuby = _ => {
+  //   console.log("click ruby working");
+  //   let ruby = this.state.ruby;
+  //   let userScore = this.state.userScore;
+  //   this.setState({
+  //     ruby: ruby,
+  //     userScore: userScore + ruby
+  //   });
+  //   console.log(ruby);
+  // };
 
   handleClickRuby = _ => {
     console.log("click ruby working");
     let ruby = this.state.ruby;
     let userScore = this.state.userScore;
+    let gameScore = this.state.gameScore;
+    let wins = this.state.wins;
+    let losses = this.state.losses;
     this.setState({
       ruby: ruby,
       userScore: userScore + ruby
     });
     console.log(ruby);
+    if (userScore === gameScore) {
+      this.setState({
+        wins: wins + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+    } else if (userScore > gameScore) {
+      this.setState({
+        losses: losses + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+      window.alert("you lose");
+    }
   };
+
+  // handleClickSapphire = _ => {
+  //   console.log("click sapphire working");
+  //   let sapphire = this.state.sapphire;
+  //   let userScore = this.state.userScore;
+  //   this.setState({
+  //     sapphire: sapphire,
+  //     userScore: userScore + sapphire
+  //   });
+  //   console.log(sapphire);
+  // };
 
   handleClickSapphire = _ => {
     console.log("click sapphire working");
     let sapphire = this.state.sapphire;
     let userScore = this.state.userScore;
+    let gameScore = this.state.gameScore;
+    let wins = this.state.wins;
+    let losses = this.state.losses;
     this.setState({
       sapphire: sapphire,
       userScore: userScore + sapphire
     });
     console.log(sapphire);
+    if (userScore === gameScore) {
+      this.setState({
+        wins: wins + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+    } else if (userScore > gameScore) {
+      this.setState({
+        losses: losses + 1,
+        userScore: 0,
+        gameScore: Math.floor(Math.random() * 20 + 1)
+      });
+      window.alert("you lose");
+    }
+  };
+
+  handleUserScore = _ => {
+    console.log("click userScore working");
+    let userScore = this.state.userScore;
+    let gameScore = this.state.gameScore;
+    let wins = this.state.wins;
+    let losses = this.state.losses;
+    if (userScore === gameScore) {
+      this.setState({
+        wins: wins + 1
+      });
+    } else if (userScore > gameScore) {
+      this.setState({
+        losses: losses + 1
+      });
+      window.alert("you lose");
+    }
   };
 
   render() {
