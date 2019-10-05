888b6fb008029b3a603d4a326a1d6d5833969dde
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-10-04 20:39:19 -0700

    done styling gameScore

--- src/Components/GameScore/GameScore.js
+++ src/Components/GameScore/GameScore.js
@@ -8,8 +8,8 @@ const GameScore = props => {
         <h4 id="gameHeader">Game Score: {props.gameScore}</h4>
       </div>
       <div id="results">
-        <h4 id="winsHeader">Wins: {props.wins}</h4>
-        <h4 id="lossesHeader">Losses: {props.losses}</h4>
+        <h4 id="winsHeader">Wins: {props.wins} | Losses: {props.losses}</h4>
+        {/* <h4 id="lossesHeader">Losses: {props.losses}</h4> */}
       </div>
     </>
   );
