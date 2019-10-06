c36044408c4bf66cb97629927aca798bb5c55490
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-10-06 16:42:28 -0700

    making gameScore more responsive in css

--- src/Components/GameScore/GameScore.js
+++ src/Components/GameScore/GameScore.js
@@ -4,15 +4,34 @@ import "./GameScore.css";
 const GameScore = props => {
   return (
     <>
-      <div id="score">
+      <div
+        className="ui stackable two column grid"
+        style={{ textAlign: "center" }}
+      >
+        <div
+          className="column"
+          style={{ backgroundColor: "green", margin: "20px", padding: "5px" }}
+        >
+          <h4>Game Score: {props.gameScore}</h4>
+        </div>
+        <div
+          className="column"
+          style={{ backgroundColor: "aqua", margin: "20px", padding: "5px" }}
+        >
+          <h4>
+            Wins: {props.wins} | Losses: {props.losses}
+          </h4>
+        </div>
+      </div>
+      {/* <div id="score">
         <h4 id="gameHeader">Game Score: {props.gameScore}</h4>
       </div>
       <div id="results">
         <h4 id="winsHeader">
           Wins: {props.wins} | Losses: {props.losses}
-        </h4>
-        {/* <h4 id="lossesHeader">Losses: {props.losses}</h4> */}
-      </div>
+        </h4> */}
+      {/* <h4 id="lossesHeader">Losses: {props.losses}</h4> */}
+      {/* </div> */}
     </>
   );
 };
